ab2c873a2973082b7db7b9ef6ea35f19e598cc24
Anjali-2707 <gurjaranjali2707@gmail.com>
2024-08-13 00:20:04 +0530

    server created

---
+++ backend/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+// const { title } = require('process');
+
+const app = express();
+
+//middleware
+app.use('/api/posts', (req, res, next) => {
+    const posts = [
+        { id: 'abcd', title: '1st post', content: 'comin from server' },
+        { id: 'efgh', title: '2nd post', content: 'comin from server!' }
+    ];
+    res.status(200).json({
+        message: 'Posts fetched succesfully',
+        posts: posts
+    });
+    next();
+});
+
+app.use((req, res, next) => {
+    //to send response
+    res.send('Hello from express')
+});
+
+module.exports = app;
